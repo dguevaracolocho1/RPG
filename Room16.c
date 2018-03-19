39965a22fdf23154995b8d1fc40a5147ea9f13a0
dguevaracolocho1 <dguevaracolocho1@dianaguevara07@outlook.com>
2018-03-19 16:58:20 -0700

    this is version 4

--- Room16.c
+++ Room16.c
@@ -4,7 +4,8 @@
 
 int main(void)
 {
-	int userInput;	
+	int userInput;
+	int trapAnswer;	
 
 	puts("Arriving in room 16, an empty cavern governed with sleeping bats, the noise of your footsteps jump off against the walls.");
 	puts("Towards the end, you can see light coming through, as if it were an exit.");
@@ -19,6 +20,14 @@ int main(void)
 		printf("Walking towards the end of the cavern, the light becomes brighter and brighter.");
 		puts("Upon entering, you are met with a room of a variety of chests. Can you trust this mysterious treasure room though?");
 		puts("Pick a number between 1-5.");
+	
+		scanf("%d", &trapAnswer);
+		
+		if((trapAnswer == 2) || (trapAnswer == 4))
+		{
+			printf("Upon opening the wooden chest, you are met with an intoxicating venoumous cloud!");
+			puts("Having inhaled the fumes, your surroundings start to become dull and your body heavy.");
+			puts("The room becomes dark as you met your doom of being greedy. Better luck next time!");
 	}
 	else if(userInput == 2)
 }
