765cc93e964e2dfef62cd8795db52eb9383e2238
dguevaracolocho1 <dguevaracolocho1@dianaguevara07@outlook.com>
2018-03-19 16:54:41 -0700

    this is version 3

--- Room16.c
+++ Room16.c
@@ -4,9 +4,21 @@
 
 int main(void)
 {
-	puts("Arriving in an empty cavern, the noise of your footsteps jump off against the walls.");
+	int userInput;	
+
+	puts("Arriving in room 16, an empty cavern governed with sleeping bats, the noise of your footsteps jump off against the walls.");
 	puts("Towards the end, you can see light coming through, as if it were an exit.");
 	puts("On your left looks to be a never ending cave room.");
 	puts("On your right you can hear what seems to be water rushing downwards.");
 	puts("Pick a number between 1-3 to see which direction you fall in.");
+	
+	scanf("%d", &userInput);
+
+	if(userInput == 1)
+	{
+		printf("Walking towards the end of the cavern, the light becomes brighter and brighter.");
+		puts("Upon entering, you are met with a room of a variety of chests. Can you trust this mysterious treasure room though?");
+		puts("Pick a number between 1-5.");
+	}
+	else if(userInput == 2)
 }
