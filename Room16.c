ed15282b0cd5a22d519031d3c23a8c544fb4534c
dguevaracolocho1 <dguevaracolocho1@dianaguevara07@outlook.com>
2018-03-19 16:43:39 -0700

    this is version 2

--- Room16.c
+++ Room16.c
@@ -4,5 +4,9 @@
 
 int main(void)
 {
-
+	puts("Arriving in an empty cavern, the noise of your footsteps jump off against the walls.");
+	puts("Towards the end, you can see light coming through, as if it were an exit.");
+	puts("On your left looks to be a never ending cave room.");
+	puts("On your right you can hear what seems to be water rushing downwards.");
+	puts("Pick a number between 1-3 to see which direction you fall in.");
 }
