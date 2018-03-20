13de85144c05f170aa75b4a1cd2388570dd5b2bb
dguevaracolocho1 <dguevaracolocho1@dianaguevara07@outlook.com>
2018-03-19 17:01:36 -0700

    this is version 5

--- Room16.c
+++ Room16.c
@@ -5,7 +5,8 @@
 int main(void)
 {
 	int userInput;
-	int trapAnswer;	
+	int trapAnswer;
+	int userHealth = 50;	
 
 	puts("Arriving in room 16, an empty cavern governed with sleeping bats, the noise of your footsteps jump off against the walls.");
 	puts("Towards the end, you can see light coming through, as if it were an exit.");
@@ -28,6 +29,15 @@ int main(void)
 			printf("Upon opening the wooden chest, you are met with an intoxicating venoumous cloud!");
 			puts("Having inhaled the fumes, your surroundings start to become dull and your body heavy.");
 			puts("The room becomes dark as you met your doom of being greedy. Better luck next time!");
+		}
+		else if((trapAnswer == 1) || (trapAnswer == 3))
+		{
+			printf("Opening the next raggedy wooden chest, your vision is obstructed by the flock of bats coming out!");
+			puts("You put your arms up to protect yourself, resulting in a multitude of bites a scratches.");
+			puts("The bats quickly leave the room. -10 HP for the wounds received.");
+			
+			userHealth = userHealth - 10;
+		}
 	}
 	else if(userInput == 2)
 }
